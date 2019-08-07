b861ee860200d14acb531bc891faa44e60f7dfb9
HKCHAT <info@hkchat.org>
2019-08-07 11:55:56 +0800

    Nothing Important

--- master/sources/auth.js
+++
@@ -1,9 +0,0 @@
-const C = {};
-C.isLoggedIn = (req, res, next) => {
-  if (req.session && req.session.user_id) return next();
-  var redr = Buffer.from(
-    req.protocol + "://" + req.get("host") + req.originalUrl
-  ).toString("base64");
-  res.redirect("/login?redr=" + redr);
-};
-module.exports = C;
